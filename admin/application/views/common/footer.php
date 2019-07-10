e17e032b78623de2bcc4f51e3d16a8b9213350d0
binnyrai <binnyrai111@gmail.com>
2019-07-10 02:23:22 -0400

    add

--- admin/application/views/common/footer.php
+++ admin/application/views/common/footer.php
@@ -1,12 +1,48 @@
 <?php
-      if(check_admin_authentication()){ 
-         
+          // if($this->session->userdata('EmailAddress'))
+          // {
   ?>
   <footer class="footer footer-static footer-light navbar-border" style="position: fixed;bottom: 0;width: 100%;">
       <p class="clearfix text-muted text-sm-center mb-0 px-2"><span class="float-md-left d-xs-block d-md-inline-block">Copyright  &copy; <?php echo date('Y');?> <a href="https://themeforest.net/user/pixinvent/portfolio?ref=pixinvent" target="_blank" class="text-bold-800 grey darken-2">  Bluegrey Technologies  </a>, All rights reserved. </span>
         <!--span class="float-md-right d-xs-block d-md-inline-block">Hand-crafted & Made with <i class="icon-heart5 pink"></i></span--></p>
     </footer>
 <?php
-  }
+  //}
 ?>
+    <!-- BEGIN VENDOR JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.js" type="text/javascript" ></script>
    
+    <script src="<?php echo base_url(); ?>default/js/core/libraries/jquery.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>default/vendors/js/ui/tether.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>default/js/core/libraries/bootstrap.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>default/vendors/js/ui/perfect-scrollbar.jquery.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>default/vendors/js/ui/unison.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>default/vendors/js/ui/blockUI.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>default/vendors/js/ui/jquery.matchHeight-min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>default/vendors/js/ui/screenfull.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>default/vendors/js/extensions/pace.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>default/js/jquery.dataTables.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>default/js/jquery.validate.min.js"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
+    <!-- BEGIN VENDOR JS-->
+    <!-- BEGIN PAGE VENDOR JS-->
+    <!-- END PAGE VENDOR JS-->
+    <!-- BEGIN ROBUST JS-->
+    <script src="<?php echo base_url(); ?>default/js/core/app-menu.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>default/js/core/app.js" type="text/javascript"></script>
+
+    
+    <!-- END ROBUST JS-->
+    <!-- BEGIN PAGE LEVEL JS-->
+    <!-- END PAGE LEVEL JS-->
+  </body>
+
+<!-- Mirrored from pixinvent.com/free-bootstrap-template/robust-lite/html/ltr/login-simple.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 04 Apr 2019 06:04:47 GMT -->
+</html>
+<SCRIPT>
+    $(document).ready(function() {
+    $('#example').DataTable( {
+       
+    } );
+} );
+</SCRIPT>
