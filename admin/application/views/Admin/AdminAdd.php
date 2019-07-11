b04432d61e4737d0877407aa7cf0ea6bdf004119
binnyrai <binnyrai111@gmail.com>
2019-07-11 01:27:18 -0400

    add

--- admin/application/views/Admin/AdminAdd.php
+++ admin/application/views/Admin/AdminAdd.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	 $this->load->view('common/header');
 	 $this->load->view('common/sidebar');
 ?>
- <div class="app-content content container-fluid">
+  <div class="app-content content container-fluid">
     <div class="content-wrapper">
         
       <div class="content-body"><!-- Basic form layout section start -->
@@ -14,121 +14,92 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<div class="col-md-12">
 			<div class="card">
 				<div class="card-header">
-					<h4 class="card-title" id="basic-layout-form">
-					<?php if($AdminId==1)
-					{
-						echo	"Edit Admin";
-					}
-					else{
-					echo	"Add Admin";
-					}
-					?>
-					<a class="heading-elements-toggle"><i class="icon-ellipsis font-medium-3"></i></a>
-					<a href="<?php echo base_url();?>Adminuser/Adminlist/" class="btn btn-primary" style="float:right">Back to Admin List</a>
+						<h4 class="card-title" id="basic-layout-form">Add Admin
+						<a class="heading-elements-toggle"><i class="icon-ellipsis font-medium-3"></i></a>
+						<a href="AdminList" class="btn btn-primary" style="float:right;">Back to Admin list</a>
+					</h4>
+					
 				</div>
-				</h4>
+				
 				<div class="card-body collapse in">
 					<div class="card-block">
-				
-						<form class="form" method="post" enctype="multipart/form-data" id="form_assesment" action="<?php echo base_url();?>Adminuser/Adminadd">
-					
+						<form class="form" method="post" enctype="multipart/form-data" id="add_admin">
 							<div class="form-body">
-								<h4 class="form-section"><i class="icon-clipboard4"></i> Requirements</h4>
-							
-								<div class="form-group">
+								<h4 class="form-section"><i class="icon-clipboard4"></i>Admin</h4>
+
+									<div class="form-group">
 								<input type="hidden"   value="<?php echo $AdminId; ?>" name="AdminId">
 									<label>Full Name</label>
 									<input type="text" class="form-control" placeholder="Full Name" name="FullName" value="<?php echo $FullName;?>" minlength="5" maxlength="200">
 								</div>
 
+								
+
+									<div class="form-group">
+									<label>Mobile No.</label>
+									<input type="text" class="form-control" placeholder="Mobile no." name="AdminContact" value="<?php echo $AdminContact;?>" minlength="5" maxlength="200">
+								</div>
 								<div class="form-group">
 									<label>Email Address</label>
 									<input type="text" class="form-control" placeholder="Email Address" name="EmailAddress" value="<?php echo $EmailAddress;?>" minlength="5" maxlength="200">
 								</div>
-
 								<div class="form-group">
-									<label>Address</label>
-									<input type="text" class="form-control" placeholder="Address" name="Addresses" value="<?php echo $Addresses;?>" minlength="5" maxlength="200">
+									<label>Password</label>
+									<input type="text" class="form-control" placeholder="Email Address" name="EmailAddress" value="<?php echo $EmailAddress;?>" minlength="5" maxlength="200">
+								</div>
+								<div class="form-group">
+									<label>Confrim password</label>
+									<input type="text" class="form-control" placeholder="Email Address" name="EmailAddress" value="<?php echo $EmailAddress;?>" minlength="5" maxlength="200">
 								</div>
 
 								<div class="form-group">
 									<label>Profile Image</label>
-									<input type="text" class="form-control" placeholder="Profile Image" name="ProfileImage" value="<?php echo $ProfileImage;?>" minlength="5" maxlength="200">
+									<input type="file" class="form-control" placeholder="Profile Image" name="ProfileImage" value="<?php echo $ProfileImage;?>" minlength="5" maxlength="200">
 								</div>
 
-								<div class="form-group">
-									<label>Contact</label>
-									<input type="text" class="form-control" placeholder="Contact" name="AdminContact" value="<?php echo $AdminContact;?>" minlength="5" maxlength="200">
-								</div>
-
-								<?php  if($IsActive!=''){ ?>
-                                
-								<div class="form-group">
-																				<label>Status</label>
-																				<div class="input-group">
-																								<label class="display-inline-block custom-control custom-radio ml-1">
-																								<?php //echo $IsActive; ?>
-																												<input type="radio" name="IsActive" value="Active"
-		<?php if($IsActive=="Active") { echo "checked"; } ?>
-		 class="custom-control-input">
-																												<span class="custom-control-indicator"></span>
-																												<span class="custom-control-description ml-0">Active</span>
-																								</label>
-																								<label class="display-inline-block custom-control custom-radio">
-																												<input type="radio" name="IsActive" value="Inactive"  <?php if($IsActive=="Active") { echo "checked"; } ?>                                                  
-		class="custom-control-input">
-																												<span class="custom-control-indicator"></span>
-																												<span class="custom-control-description ml-0">Inactive</span>
-																								</label>
-																				</div>
-								</div>
-				<?php } else { ?>
-								<div class="form-group">
-																				<label>Status</label>
-																				<div class="input-group">
-																								<label class="display-inline-block custom-control custom-radio ml-1">                                                                                
-																												<input type="radio" name="IsActive" value="Active"
-	 checked="" 
-		 class="custom-control-input">
-																												<span class="custom-control-indicator"></span>
-																												<span class="custom-control-description ml-0">Active</span>
-																								</label>
-																								<label class="display-inline-block custom-control custom-radio">
-																												<input type="radio" name="IsActive" value="Inactive"
-		class="custom-control-input">
-																												<span class="custom-control-indicator"></span>
-																												<span class="custom-control-description ml-0">Inactive</span>
-																								</label>
-																				</div>
-								</div>
-
-				<?php }?>
-
-
-							<div class="form-actions">
-					
-								<?php if($AdminId!=''){?>
-									<button type="submit" name="updateAdmin" class="btn btn-primary">
-										<i class="icon-check2"></i> Update
-									</button>
-								<?php }else{ ?>
-									<button type="submit" name="addAdmin" class="btn btn-primary">
-									<i class="icon-check2"></i> Add
-									</button>
-								<?php } ?>
 								
-							
-								<a href="<?php echo base_url(); ?>Adminuser/Adminlist" name="CancelAdmin" class="btn btn-danger">
-								Cancel
-								</a>
+								<div class="form-group">
+									<label>Address</label>
+									<textarea type="text" class="form-control" placeholder="Address"  name="Address" id="Address" ></textarea>
+								</div>
+
+								<!-- <div class="form-group ">
+									<label>Profile Image</label>
+									<input type="hidden" value="<?php //echo  $ProfileImage ?>" name="pre_profile_image" >
+									<p><span class="btn btn-primary btn-file">
+									Upload profile image <input type="file" name="ProfileImage" id="profileimage" onchange="readURL(this);">
+									</span></p>									
+									<span id="profileerror"></span>
+								</div> -->
+								
+								<div class="preview">
+									<img id="blah" src="" class="img-thumbnail border-0" style="display: none;  width: 100px; height: 100px;">
+								</div>
+
+								<div class="form-group">
+											<label>Status</label>
+											<div class="input-group">
+												<label class="display-inline-block custom-control custom-radio ml-1">
+													<input type="radio" name="IsActive" value="1" checked="" class="custom-control-input">
+													<span class="custom-control-indicator"></span>
+													<span class="custom-control-description ml-0">Active</span>
+												</label>
+												<label class="display-inline-block custom-control custom-radio">
+													<input type="radio" name="IsActive" value="0" class="custom-control-input">
+													<span class="custom-control-indicator"></span>
+													<span class="custom-control-description ml-0">Inactive</span>
+												</label>
+											</div>
+								</div>
+								<div class="form-actions">
+								<input  type="submit" name="save" class="btn btn-primary" value="Add">
+							</div>
 							</div>
 						</form>
 					</div>
 				</div>
 			</div>
 		</div>
-
-		
 			
 	</div>
 </section>
@@ -136,56 +107,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
       </div>
     </div>
-	<?php 
- $this->load->view('common/footer');
-?>
-
-<script>
-
-$(document).ready(function()
-{
-		$("#form_assesment").validate(
-		{
-			rules: {
-
-						StreamTypeId: {
-								required: true,
-													},
-						ProgramId: {
-								required: true,
-													},
-						AssesmentName: {
-								required: true,
-							//	pattern: /^[a-zA-Z0-9\s\-\ ]+$/,
-							//	minlength: 5,
-													},
-
-						},
-
-						messages: {
-
-						StreamTypeId: {
-						required: "Plesae select stream",
-
-										},
-						ProgramId: {
-						required: "Plesae select program",
-
-										},
-						AssesmentName: {
-						required: "Please enter assesment name",
-						pattern : "Enter only characters and numbers and \"space , \" -",
-						minlength: "Please enter at least 5 and maximum to 200 letters!",
-										},
-					
-					
-						}
-				
-		});
-});
-
- 
-					                    CKEDITOR.replace('editor1');
-					                
-
-</script>
+ <script>
+ 	 function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    $('#blah').css('display', 'block');
+                    $('#blah').attr('src', e.target.result);
+                };
+             reader.readAsDataURL(input.files[0]);
+            }
+        }
+ </script>
---
+++ admin/logs/log-2019-07-11.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-07-11 07:15:25 --> 404 Page Not Found: Favicon/favicon-32.png
+ERROR - 2019-07-11 07:15:26 --> 404 Page Not Found: Favicon/favicon.ico
+ERROR - 2019-07-11 07:15:44 --> 404 Page Not Found: App-assets/images
+ERROR - 2019-07-11 07:15:44 --> 404 Page Not Found: Home/app-assets
+ERROR - 2019-07-11 07:15:46 --> 404 Page Not Found: App-assets/images
+ERROR - 2019-07-11 07:16:30 --> 404 Page Not Found: Admin/Adminadd
+ERROR - 2019-07-11 07:16:48 --> 404 Page Not Found: Admin/Adminadd
+ERROR - 2019-07-11 07:16:51 --> 404 Page Not Found: Favicon/favicon-32.png
+ERROR - 2019-07-11 07:16:51 --> 404 Page Not Found: Favicon/favicon.ico
+ERROR - 2019-07-11 07:16:52 --> 404 Page Not Found: App-assets/images
+ERROR - 2019-07-11 07:17:18 --> 404 Page Not Found: App-assets/images
+ERROR - 2019-07-11 07:17:18 --> 404 Page Not Found: Favicon/favicon-32.png
+ERROR - 2019-07-11 07:17:18 --> 404 Page Not Found: Favicon/favicon.ico
