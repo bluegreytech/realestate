ca69e3db65ae93e96ee7b1dfc7e4c3b97be08349
Binny29 <52149842+Binny29@users.noreply.github.com>
2019-08-21 10:11:07 +0530

    add

---
+++ admin/logs/log-2019-08-20.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-08-20 13:28:49 --> 404 Page Not Found: Default/images
+ERROR - 2019-08-20 13:28:49 --> 404 Page Not Found: Default/images
+ERROR - 2019-08-20 13:28:49 --> 404 Page Not Found: Favicon/favicon-32.png
+ERROR - 2019-08-20 13:28:49 --> 404 Page Not Found: Favicon/favicon.ico
+ERROR - 2019-08-20 14:49:10 --> 404 Page Not Found: Default/images
+ERROR - 2019-08-20 14:49:10 --> 404 Page Not Found: Default/images
+ERROR - 2019-08-20 14:49:10 --> 404 Page Not Found: Favicon/favicon-32.png
+ERROR - 2019-08-20 14:49:10 --> 404 Page Not Found: Favicon/favicon.ico
