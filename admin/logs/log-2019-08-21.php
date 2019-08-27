97b16b2cf5709f724a0ea3c66b9f475975fb0bf3
Mitesh Patel <mitesh@bluegreytech.com>
2019-08-27 04:57:27 -0400

    add

---
+++ admin/logs/log-2019-08-21.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-08-21 13:17:17 --> 404 Page Not Found: Favicon/favicon-32.png
+ERROR - 2019-08-21 13:17:17 --> 404 Page Not Found: Favicon/favicon.ico
+ERROR - 2019-08-21 13:17:50 --> Query error: Table 'realstatesdb.tblemail_template' doesn't exist - Invalid query: select * from tblemail_template where task='Forgot Password by admin'
+ERROR - 2019-08-21 13:19:16 --> Query error: Table 'realstatesdb.tblemail_setting' doesn't exist - Invalid query: SELECT *
+FROM `tblemail_setting`
+WHERE `email_setting_id` = 1
+ERROR - 2019-08-21 13:21:01 --> 404 Page Not Found: Favicon/favicon-32.png
+ERROR - 2019-08-21 13:21:01 --> 404 Page Not Found: Favicon/favicon.ico
